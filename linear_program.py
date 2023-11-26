7879b515d7fc9d15ea5cf9c308779e7b5d1fbd7a
Liam Swayne <liamtswayne@gmail.com>
2023-11-26 16:50:52 -0500

    added costs

--- linear_program.py
+++ linear_program.py
@@ -24,17 +24,19 @@ def calculate(floors=1, xLength=1, yLength=1):
     
     # create columns
     woodColumns = cp.Variable((xLength,yLength), integer = True)
-    steelReinforcedColumns = cp.Variable((xLength,yLength), integer = True)
+    steelColumns = cp.Variable((xLength,yLength), integer = True)
     
     # create trees
     # TODO: add more trees
     slashPineAcres = cp.Variable(nonneg = True)
     
-    # cost calculation
+    # cost calculation (measured in USD)
     # page 3 of https://bugwoodcloud.org/bugwood/productivity/pdfs/SeriesPaper5.pdf for slash pine cost per acre
     cost = slashPineAcres*(55+110)
     # cost of wood columns
-    cost += cp.sum(woodColumns)*1000
+    cost += cp.sum(woodColumns)*278
+    # cost of steel columns
+    cost += cp.sum(steelColumns)*64.90
     # cost of each tile
     cost += 1000*xLength*yLength*floors
     
@@ -43,17 +45,16 @@ def calculate(floors=1, xLength=1, yLength=1):
     
     # CO2 constraint measured in metric tons
     # slash pine figure from https://extension.psu.edu/carbon-accounting-in-forest-management#:~:text=100%2C000%20pounds%20carbon%20per%20acre%20%C3%B7%2045,tons%20of%20CO2%20emissions%20avoided
-    # steel-reinforced column figure from https://www.mdpi.com/1996-1073/6/11/5609#:~:text=As%20shown%20in%20Figure%205%2C%20the,in%20Yeo%20and%20Potra%20%5B18%5D.
-    constraints.append(cp.sum(steelReinforcedColumns)*485 + cp.sum(woodColumns)*1000 - slashPineAcres*166.05 <= 0)
+    # steel column figure from https://www.mdpi.com/1996-1073/6/11/5609#:~:text=As%20shown%20in%20Figure%205%2C%20the,in%20Yeo%20and%20Potra%20%5B18%5D.
+    constraints.append(cp.sum(steelColumns)*485 + cp.sum(woodColumns)*1000 - slashPineAcres*166.05 <= 0)
     
     # columns supporting each tile
-    for i in range(xLength):
-        for j in range(yLength):
-            constraints.append(woodColumns[i][j]*1000 + steelReinforcedColumns[i][j]*1000 >= floors*tileWeight)
+    # steelColumn support figure from https://www.homedepot.com/p/Tiger-Brand-8-ft-to-8-ft-4-in-Adjustable-Steel-Building-Support-Column-3-in-O-D-3A-8084/202086528#:~:text=maximum%20extension%20(lb.)-,11200%20lb,-Maximum%20load%20at
+    constraints.append(cp.sum(woodColumns)*1000 + cp.sum(steelColumns)*5.0802345 >= floors*tileWeight)
     
     # nonnegativity
     constraints.append(woodColumns >= 0)
-    constraints.append(steelReinforcedColumns >= 0)
+    constraints.append(steelColumns >= 0)
     
     # create and solve problem
     problem = cp.Problem(cp.Minimize(cost), constraints)
@@ -64,7 +65,7 @@ def calculate(floors=1, xLength=1, yLength=1):
     logs.append(cost.value)
     logs.append("\ncolumns (measured in quantity):")
     logs.append("Wood columns:\n"+str(woodColumns.value))
-    logs.append("Steel columns:\n"+str(steelReinforcedColumns.value))
+    logs.append("Steel columns:\n"+str(steelColumns.value))
     logs.append("\ncarbon offsets (measured in acres):")
     logs.append(slashPineAcres.value)
 
